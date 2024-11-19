18d4ff065b679998b88b1ba2291ce6e6263196a9
De'Jean Williams <dejeanwilliams2003@gmail.com>
2024-11-19 07:56:04 -0800

    commit

--- src/com/perscholas/java_basics/Main.java
+++ src/com/perscholas/java_basics/Main.java
@@ -7,30 +7,37 @@ public class Main {
         int i = 5;
         int sum = j + i;
         System.out.println(sum);
+
         double z = 3.14;
         double d = 2.56;
         double sum2 = z + d;
         System.out.println(sum2);
+
         int a = 3;
         double b = 6.78;
         double sum3 = a + b;
         System.out.println(sum3);
+
         double c = 6.1;
         int f = 3;
         int quote = (int) (c / f);
         System.out.println(quote);
+
         double o = 8.98;
         double n = 7.45;
         int quote1 = (int) (o / n);
         System.out.println(quote1);
+
         int x = 5;
         double y = 6.1;
         int q = (int) (y / x);
         System.out.println(q);
+
         final int MAX_VALUE = Integer.MAX_VALUE;
         final int MIN_VALUE = Integer.MIN_VALUE;
         System.out.println(MAX_VALUE);
         System.out.println(MIN_VALUE);
+
         double coffee = 6.50;
         double cappuccino = 7.25;
         double tea = 5.75;
